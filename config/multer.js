c0b5393b4f9c98e6334d8385fec734e75a90739d
DarshanDodal <darshan.dodal@gmail.com>
2024-05-05 10:29:55 +0530

    Multer config updated and errorHandler updated

--- config/multer.js
+++ config/multer.js
@@ -1,5 +1,6 @@
 const multer = require("multer");
 const path = require("path");
+require("dotenv").config();
 
 // Multer configuration
 const storage = multer.diskStorage({
@@ -11,6 +12,23 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: process.env.MAX_UPLOAD_SIZE, // 10MB
+  },
+  fileFilter: (req, file, cb) => {
+    const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
+
+    if (!allowedTypes.includes(file.mimetype)) {
+      const error = new Error("Invalid file type");
+      error.code = "INVALID_FILE_TYPE";
+      error.message = "Incorrect file. Please use .jpeg or .png files.";
+      return cb(error, false);
+    }
+
+    cb(null, true);
+  },
+});
 
 module.exports = upload;
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,5 +2,5 @@
 
 module.exports = (err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send({ success: false, error: err.stack });
+  res.status(500).send({ message: err.stack });
 };
